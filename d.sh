c8906bbc69d3dae96b9ec785efd952ec09246ded
mauricio <mauricio.vera.r7@gmail.com>
2022-04-13 15:04:10 -0400

    gen deploy

---
+++ d.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#**agregar estas  lineas en nuxt.config debajo de // Global
+#export default {
+#  // Global page headers: https://go.nuxtjs.dev/config-head
+#  target: 'static',
+#  router: {
+#    base: '/phrases/'
+#  },
+#Instalamos la librería push-dir que nos permitirá realizar el deploy
+#npm install push-dir --save-dev
+#**agregar deploy a json
+# "start": "nuxt start",
+#  "deploy": "push-dir --dir=dist --branch=gh-pages --cleanup",
+#}
+#**subir cambios a github
+#npm run generate   (nuxt generate) (esto genera dist directorio)
+#npm run deploy
+#habilitar gh-pages en github
+#repositorio debe ser publico, no privado
+
+git add .
+git commit -m 'gen deploy'
+git push
+
+npm run generate
+npm run deploy
+# Este es un comentario en bash  ./d.sh

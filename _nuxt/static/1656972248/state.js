0a22e5f3104bd2ce33bf796c9c915ee3b74f81b8
Mauriciovr7 <52289594+Zentella@users.noreply.github.com>
2022-07-04 18:04:17 -0400

    57d6055

---
+++ _nuxt/static/1656972248/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffrasesnuxt\u002F_nuxt\u002Fstatic\u002F1656972248",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffrasesnuxt\u002F",assetsPath:"\u002Ffrasesnuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
